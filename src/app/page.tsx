cf6dc25e0b2f5169533e0dd9eb4ad34e5cda366e
dtr00 <dangtr0212@gmail.com>
2026-02-28 15:45:59 -0500

    feat: add Intro section - adjust heading to fit intro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import { GithubContributions } from "@/components/github/github-contributions";
+import { Intro } from "@/components/intro";
 
 export default function Page() {
   return (
     <>
+      <Intro />
+
       <GithubContributions />
     </>
   );
---
+++ src/components/intro.tsx
@@ -0,0 +1,10 @@
+export function Intro() {
+  return (
+    <section className="pb-10">
+      <div>hi, i&apos;m dang</div>
+      <div className="text-base/5 mt-1.5">
+        i like building things with fluidity and for real-time efficiency
+      </div>
+    </section>
+  );
+}
